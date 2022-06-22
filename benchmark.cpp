ea8522a27b8244f6584ef51c74f8b2748397d8cf
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:04:46 +0400

    Add benchmark with nlohmann-json

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,9 +1,11 @@
 #include "Value.h"
 
+#include <boost/json.hpp>
 #include <chrono>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
 #include <sstream>
 #include <string>
 
@@ -41,7 +43,43 @@ int benchVehlwn()
     return 0;
 }
 
+int benchBoost()
+{
+    std::ofstream of{"times.txt"};
+    for(std::size_t i = 100; i <= 10'000'000; i += 100'000)
+    {
+        const std::string input = genStr(i);
+        const double mb = input.size() / 1024. / 1024.;
+        std::cout << "i = " << i << "; len = " << mb << " MB" << std::endl;
+        const auto t1 = std::chrono::steady_clock::now();
+        const auto value = boost::json::parse(input);
+        const auto t2 = std::chrono::steady_clock::now();
+        const double secs = std::chrono::duration<double>(t2 - t1).count();
+        of << mb << '\t' << secs << std::endl;
+        if(!value.is_array())
+            return 1;
+    }
+    return 0;
+}
+int benchNlohmann()
+{
+    std::ofstream of{"times.txt"};
+    for(std::size_t i = 100; i <= 10'000'000; i += 100'000)
+    {
+        const std::string input = genStr(i);
+        const double mb = input.size() / 1024. / 1024.;
+        std::cout << "i = " << i << "; len = " << mb << " MB" << std::endl;
+        const auto t1 = std::chrono::steady_clock::now();
+        const auto value = nlohmann::json::parse(input);
+        const auto t2 = std::chrono::steady_clock::now();
+        const double secs = std::chrono::duration<double>(t2 - t1).count();
+        of << mb << '\t' << secs << std::endl;
+        if(!value.is_array())
+            return 1;
+    }
+    return 0;
+}
 int main()
 {
-    return benchVehlwn();
+    return benchBoost();
 }
