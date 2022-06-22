151f629ae5088619dc97185db38f8db7d71306d5
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:04:45 +0400

    Add gnuplot script for time visualisation

--- benchmark.cpp
+++ benchmark.cpp
@@ -2,31 +2,46 @@
 
 #include <chrono>
 #include <cstddef>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-int main()
+std::string genStr(const std::size_t n)
 {
     std::ostringstream os;
     os << "[";
     std::string comma = "";
-    for(std::size_t i = 0; i < 3'000; i++)
+    for(std::size_t i = 0; i < n; i++)
     {
         os << comma << "{"
            << "\"key " << i << "\":" << i << "}";
         comma = ",";
     }
     os << "]";
-    std::string input = os.str();
-    const double mb = input.size() / 1024. / 1024.;
-    std::cout << "size = " << mb << " MB" << std::endl;
-    using vehlwn::json::parse;
-    const auto t1 = std::chrono::steady_clock::now();
-    const auto value = parse(input);
-    const auto t2 = std::chrono::steady_clock::now();
-    std::cout << "isArray = " << value.isArray() << std::endl;
-    const double secs = std::chrono::duration<double>(t2 - t1).count();
-    std::cout << secs << " s" << std::endl;
-    std::cout << mb / secs << " MB/s" << std::endl;
+    return os.str();
+}
+
+int benchVehlwn()
+{
+    std::ofstream of{"times.txt"};
+    for(std::size_t i = 100; i <= 30'000; i += 500)
+    {
+        const std::string input = genStr(i);
+        std::cout << "i = " << i << "; len = " << input.size() << " bytes"
+                  << std::endl;
+        const auto t1 = std::chrono::steady_clock::now();
+        const auto value = vehlwn::json::parse(input);
+        const auto t2 = std::chrono::steady_clock::now();
+        const double secs = std::chrono::duration<double>(t2 - t1).count();
+        of << input.size() << '\t' << secs << std::endl;
+        if(!value.isArray())
+            return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    return benchVehlwn();
 }
